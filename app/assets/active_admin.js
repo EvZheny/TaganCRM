8aa3e932f27720c1a66e9353ea1c33a55467d345
EvZheny <chertckov@gmail.com>
2017-08-02 15:27:37 +0300

    Fixed views

--- app/assets/active_admin.js
+++
@@ -1,2 +0,0 @@
-//= require jquery-migrate
-//= require active_admin/base
--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -4,15 +4,21 @@ class ExercisesController < ApplicationController
 
   def index
     @exercises = Exercise.all
+    @view_exercises = []
+    @exercises.each do
+      |ex| @view_exercises << new_exercise(ex)
+    end
   end
 
   def show
-    @exercise = Exercise.where(id: params[:id]).first
-    render "show"
+    @exercise = Exercise.find(params[:id])
+    @view_exercises = new_exercise(@exercise)
   end
 
   def new
     @exercise = Exercise.new
+    @users = User.all
+    @exercise_lists = ExerciseList.all
   end
 
   def create
@@ -26,6 +32,14 @@ class ExercisesController < ApplicationController
 
   def exercise_params
     params.require(:exercise).permit(:exercise_list_id, :user_name_id, :weight)
+  end
+
+  def new_exercise (ex={})
+    new_exercise = {
+        id:       ex.id,
+        exercise: ExerciseList.find(ex.exercise_list_id).exercise,
+        user:     User.find(ex.user_name_id).first_name + " " + User.find(ex.user_name_id).last_name,
+        weight:   ex.weight}
   end
 
 end
